50379bf075f353ebb09e847a7bfda4279c707a1f
gobGrobw <albertfoeng12@gmail.com>
2024-06-04 15:27:55 +0700

    set title to center

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,7 +3,7 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 export default function Auth() {
 	return (
 		<main className="flex gap-10 justify-center items-center h-full flex-col">
-			<h1 className="text-4xl font-bold">Easy way to manage your task</h1>
+			<h1 className="text-4xl font-bold text-center">Easy way to manage your task</h1>
 			<nav className="text-2xl">
 				<SignedOut>
 					<SignInButton className="hover:bg-neutral-200 pl-2 pr-2 rounded-lg" />
